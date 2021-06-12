0333319bf879e3724ad624354dc84d434997a583
Niels Smith <30351969+smithniels@users.noreply.github.com>
2021-06-12 12:59:36 +0200

    Danish Quiz App Day 1

---
+++ final/src/Questions.js
@@ -0,0 +1,94 @@
+const Questions = [
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "Text should go here?",
+    answerOptions: [
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: false },
+      { answerText: "", isCorrect: true },
+      { answerText: "", isCorrect: false },
+    ],
+  },
+];
+
+export default Questions;
